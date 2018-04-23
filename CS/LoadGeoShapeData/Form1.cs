c7ee531eafe6d3a8f0d5d3bb4b2483b347829d7c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LoadGeoShapeData/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraMap;
+
+namespace LoadGeoShapeData {
+    public partial class Form1 : Form {
+        List<MapData> data = new List<MapData>();
+        ShapefileDataAdapter adapter = new ShapefileDataAdapter();
+        
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Uri baseUri = new Uri(System.Reflection.Assembly.GetEntryAssembly().Location);
+            #region #AutomaticallyLoaded
+            data.Add(new MapData() {
+                Name = "Automatically loaded coordinate system",
+                FileUri = new Uri(baseUri, "../../Shapefiles/Albers/switzerland.shp")
+            });
+            #endregion #AutomaticallyLoaded
+            #region #LoadPrjFile
+            data.Add(new MapData() {
+                Name = "LoadPrjFile( ) calling loaded coordinate system",
+                FileUri = new Uri(baseUri, "../../Shapefiles/Lambert/Belize.shp"),
+                CoordinateSystem = ShapefileDataAdapter.LoadPrjFile(new Uri(
+                    baseUri,
+                    "../../Shapefiles/Lambert/Projection.prj"))
+            });
+            #endregion #LoadPrjFile
+            #region #ManuallyCreated
+            data.Add(new MapData() {
+                Name = "Manually created coordinate system",
+                FileUri = new Uri(baseUri, "../../Shapefiles/TransverseMercator/israel.shp"),
+                CoordinateSystem = new CartesianSourceCoordinateSystem() {
+                    CoordinateConverter = new UTMCartesianToGeoConverter() {
+                        Hemisphere = Hemisphere.Northern, 
+                        UtmZone = 36
+                    }
+                }
+            });
+            #endregion #ManuallyCreated
+
+            cbCoordinateSystem.DataSource = data;
+            cbCoordinateSystem.DisplayMember = "Name";
+            
+            VectorItemsLayer layer = new VectorItemsLayer() { Data = adapter };
+            layer.ItemStyle.Fill = Color.FromArgb(60, 255, 128, 0);
+            layer.DataLoaded += layer_DataLoaded;
+
+            mapControl1.Layers.Add(layer);
+        }
+
+        void layer_DataLoaded(object sender, DataLoadedEventArgs e) {
+            mapControl1.ZoomToFitLayerItems(0.4);
+        }
+
+        private void cbCoordinateSystem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MapData mapData = cbCoordinateSystem.SelectedValue as MapData;
+            if (mapData == null) return;
+
+            adapter.FileUri = mapData.FileUri;
+            if (mapData.CoordinateSystem != null)
+                adapter.SourceCoordinateSystem = mapData.CoordinateSystem;
+        }
+    }
+
+    public class MapData {
+        public String Name { get; set; }
+        public Uri FileUri { get; set; }
+        public SourceCoordinateSystem CoordinateSystem { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# How to: automatically load a Cartesian data shapefile onto a geo map
+
+
+This example demonstrates how to automatically load a Cartesian data shapefile onto a Geo map.
+
+
+<h3>Description</h3>
+
+<p>To automatically load Cartesian map data from a shapefile onto a Geo map do the following.</p>
+<p>- If the shapefile data contains a <em>*.PRJ</em> file with the same name and path as a <em>*.SHP</em> file, specify the&nbsp;<a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapShapefileDataAdapter_FileUritopic">ShapefileDataAdapter.FileUri</a> property. The data will be loaded automatically.</p>
+<p>- Otherwise, to load coordinate system information if the file names or paths&nbsp;are different. Use the&nbsp;<a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapShapefileDataAdapter_LoadPrjFiletopic">ShapefileDataAdapter.LoadPrjFile</a> method to initialize the&nbsp;<a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapSourceCoordinateSystemtopic">SourceCoordinateSystem</a> class descendant object, which should be assigned to the&nbsp;<a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapCoordinateSystemDataAdapterBase_SourceCoordinateSystemtopic">CoordinateSystemDataAdapterBase.SourceCoordinateSystem</a> property of the data adapter.</p>
+
+<br/>
+
+
